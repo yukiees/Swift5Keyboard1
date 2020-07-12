035d8103933bc78fb24bffcbf034226810ae95f6
yukiees <yukiii8989@gmail.com>
2020-07-13 02:03:59 +0900

    Delegate&protocol udemy

--- Swift5Keyboard1/ViewController.swift
+++ Swift5Keyboard1/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var passWordLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameTextField.delegate = self
+        passWordTextField.delegate = self
+
     }
 
-
+    @IBAction func login(_ sender: Any) {
+        
+        logoImageView.image = UIImage(named: "loginOK")
+        
+        userNameLabel.text = userNameTextField.text
+        passWordLabel.text = passWordTextField.text
+        
+        
+    }
+    
+    //タッチで、キーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    //returnキーでキーボードを閉じる。
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
